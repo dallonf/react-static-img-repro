f00b2238e4140d17ba1011e9fcf6cd7cffda42eb
Dallon Feldner <dallon.feldner@rigup.co>
2019-10-14 10:50:00 -0500

    Added test image to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import imgSrc from '../img.png';
 
 export default () => {
   return (
     <div>
       <p>React Static is a progressive static site generator for React.</p>
+      <img src={imgSrc} />
     </div>
   );
 };
